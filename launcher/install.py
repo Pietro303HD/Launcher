b611fae3db77823f39efee7973b49f89f1d82522
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2021-07-05 20:51:47 -0300

    Update install.py

--- launcher/install.py
+++ launcher/install.py
@@ -5,10 +5,11 @@ def update(app):
 	# get all filenames on a folder
 	# rewrite/write them all.
 	for file in requests.get(f'https://api.github.com/repos/Pietro303HD/Launcher/contents/{app}/').json():
-		print(requests.get(file['download_url']).text)
 		path = file['path']
 		open(f'../{path}', 'w').write(requests.get(file['download_url']).text)
+	print(f'{app} has been updated!')
 	vers = json.load(open('../versions.json'))
 	vers[app] = requests.get('https://raw.githubusercontent.com/Pietro303HD/Launcher/master/versions.json')[app]
-	open(f'../versions.json', 'w').write(json.dumps(vers))
+	versw = open(f'../versions.json', 'w')
+	versw.write(json.dumps(vers))
 	
