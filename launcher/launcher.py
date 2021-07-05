716fc652a88236fd8217335fcfd8f4b93b7dbe58
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2021-07-05 19:21:44 -0300

    Update launcher.py

--- launcher/launcher.py
+++ launcher/launcher.py
@@ -1,4 +1,13 @@
 import requests
+import json
+local = {
+"launcher": 0.0
+"pcbot": 0.0
+}
 
 version = requests.get('https://raw.githubusercontent.com/Pietro303HD/Launcher/master/versions.json')
 print("Nothing to see here.")
+for app, ver in version.json():
+    print(f'{app} : {ver}')
+    if ver > local[app]:
+        print(f'{app} needs a update!')
