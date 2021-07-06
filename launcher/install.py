92ae3cfd8eb8c65ba6f0b302d7a6c2c1be6a39b0
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2021-07-06 12:54:49 -0300

    Add files via upload

---
+++ launcher/install.py
@@ -0,0 +1,27 @@
+import requests
+import os
+import json
+def update(app):
+
+	# get all filenames on a folder
+	# rewrite/write them all.
+
+	vers = json.load(open('../versions.json'))
+	vers[app] = requests.get('https://raw.githubusercontent.com/Pietro303HD/Launcher/master/versions.json').json()[app]
+	versw = open(f'../versions.json', 'w')
+	versw.write(json.dumps(vers))
+
+	try:
+		os.mkdir(f'../{app}') 
+	except FileExistsError:
+		pass
+
+	for file in requests.get(f'https://api.github.com/repos/Pietro303HD/Launcher/contents/{app}/').json():
+		path = file['path']
+		open(f'../{path}', 'w').write(requests.get(file['download_url']).text)
+	print(f'{app} has been updated!')
+
+	
+
+	
+
---
+++ launcher/main.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+from install import update
+
+# kkk
+
+print("""
+   ___ _      _                 __                        _               
+  / _ (_) ___| |_ _ __ ___     / /  __ _ _   _ _ __   ___| |__   ___ _ __
+ / /_)/ |/ _ \ __| '__/ _ \   / /  / _` | | | | '_ \ / __| '_ \ / _ \ '__|
+/ ___/| |  __/ |_| | | (_) | / /__| (_| | |_| | | | | (__| | | |  __/ |   
+\/    |_|\___|\__|_|  \___/  \____/\__,_|\__,_|_| |_|\___|_| |_|\___|_|
+
+""")
+
+local = json.load(open('../versions.json',))
+version = requests.get('https://raw.githubusercontent.com/Pietro303HD/Launcher/master/versions.json')
+
+for app, ver in version.json().items():
+    if ver > local[app]:
+        print(f'{app} has a update! Would you like to update it?')
+        print('Here are the change logs:')
+        print()
+        print(requests.get(f'https://raw.githubusercontent.com/Pietro303HD/Launcher/master/{app}/changelogs.txt').text)
+        print()
+        prompt = input('> ')
+        if prompt == 'y': update(app)
+  
+while True:
+    print('What would you like to do?')
+    cmd = input('> ')
+    if cmd == 'version':
+      for key, value in local.items():
+         print(f'{key} - {value}')
