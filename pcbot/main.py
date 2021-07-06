ceab91a4d2d15631657fdc3cd5548a1786691795
Pietro <47507131+Pietro303HD@users.noreply.github.com>
2021-07-05 21:52:10 -0300

    Update main.py

--- pcbot/main.py
+++ pcbot/main.py
@@ -1,1 +1,35 @@
-print("Nothing to see here yet.")
+# bot.py
+import os
+import pyautogui
+import json
+from io import BytesIO
+
+import discord
+
+settings = json.load(open('configs.json')
+token = settings['token']
+prefix = settings['prefix']
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(
+        f'https://discord.com/oauth2/authorize?client_id={client.user.id}'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == f'{prefix}move':
+        response = 'testando'
+        await message.channel.send(response)
+    if message.content == f'{prefix}screenshot':
+    	image = pyautogui.screenshot()
+    	with BytesIO() as image_binary:
+                    image.save(image_binary, 'PNG')
+                    image_binary.seek(0)
+                    await message.channel.send(file=discord.File(fp=image_binary, filename='image.png'))
+
+client.run(token)
